de30c8f345555b0c7097cd87d4b3bf49c709ca2e
Arsath-md <mdarsath.m2003@gmail.com>
2025-02-08 22:54:37 +0530

    bismillah2

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import {BrowserRouter,Routes,Route} from 'react-router-dom'
 function App() {
   return (
      <>
-      <BrowserRouter>
+      <BrowserRouter basename="/react-portfolio">
       
       <Routes>
           <Route path='/' element={<Home/>}/>
